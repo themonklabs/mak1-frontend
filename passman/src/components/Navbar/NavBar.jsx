73b1372cbc3114ebffec964ea7bfd1516b3f6dd2
thesarathpulikkal <sarathhari@gmail.com>
2022-02-10 00:14:47 -0500

    Nav bar first work effort

--- passman/src/components/Navbar/NavBar.jsx
+++ passman/src/components/Navbar/NavBar.jsx
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
 import {MenuItems} from './MenuItems'
 import './NavBar.css'
+import { Navbar, Container } from 'react-bootstrap'
+import { Divide as Hamburger } from 'hamburger-react'
+
 class NavBar extends Component {
+    state={ clicked: false }
+
+
     render() { 
         return (
-            <nav className='NavbarItem'>
-                <h1 className="navbar-logo">React</h1>
-                <div className='menu-icon'>
 
-                </div>
-                <ul>
-                    {MenuItems.map((item,index)=>{
-                        return ( 
-                        <li key={index}>
-                            <a className={item.cName} href={item.url}>
-                            {item.title}
-                            </a> 
-                        </li>)
-                    })}
-                   
-                </ul>
-            </nav>
+            <Navbar className='NavBarItems'>
+                <Hamburger />
+                <Container>
+                <Navbar.Brand href="#">Navbar</Navbar.Brand>
+                </Container>
+            </Navbar>
+
         );
     }
 }
  
 export default NavBar;
+//     render() { 
+//         return (
+//             <nav className='NavBarItems'>
+//                 <h1 className="navbar-logo">React</h1>
+//                 <div className='menu-icon' onClick={this.handleClick()}>
+//                     <i className={this.state.clicked ? 'fas':}></i>
+
+//                 </div>
+//                 <ul className="nav nav-tabs" id="myTab" role="tablist">
+//                     {MenuItems.map((item,index)=>{
+//                         return ( 
+//                         <li key={index} className='nav-item' role="presentation">
+//                             <a className={item.cName} href={item.url}>
+//                             {item.title}
+//                             </a> 
+//                         </li>)
+//                     })}
+                   
+//                 </ul>
+//             </nav>
+//         );
+//     }
+// }
+ 
+// export default NavBar;
